e5696af3408e20780b2ee9565e5f6a28dfa9f3f3
Rachel T <rachmaybe@gmail.com>
2019-08-23 16:33:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(array)
   array.any? {|x| x.length > 4}
 end
 
-def find_the_cheese
+def find_the_cheese(string)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheddar_included = strings.include? ("cheddar")
   gouda_included = strings.include? ("gouda")
