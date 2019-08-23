d816564c46cb1102aa3c8e5ec1fb8dbc1856fa8d
Rachel T <rachmaybe@gmail.com>
2019-08-23 16:36:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,20 +18,5 @@ def long_planeteer_calls(array)
   array.any? {|x| x.length > 4}
 end
 
-def find_the_cheese(string)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_included = strings.include? ("cheddar")
-  gouda_included = strings.include? ("gouda")
-  camembert_included = strings.include? ("camebert")
-
-     if cheddar_included == true
-      return "cheddar"
-    elsif gouda_included == true
-      return "gouda"
-    elsif camembert_included == true
-      return "camebert"
-    else 
-      return nil
-    end
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese
 end
