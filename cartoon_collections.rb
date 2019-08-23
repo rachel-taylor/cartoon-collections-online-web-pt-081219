89ead1ce84c8abefd68b4d336031b2d4ce3559a3
Rachel T <rachmaybe@gmail.com>
2019-08-23 16:38:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(array)
   array.any? {|x| x.length > 4}
 end
 
-def find_the_cheese
+def find_the_cheese(food)
   food.find do |num|
     num == "cheddar" || num == "gouda" || num == "camembert"
   end 
