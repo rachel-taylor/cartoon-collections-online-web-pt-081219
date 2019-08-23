5369a0e6e5c6c89a0faad221e3ee2a3584af5cec
Rachel T <rachmaybe@gmail.com>
2019-08-23 16:37:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,9 @@ def long_planeteer_calls(array)
   array.any? {|x| x.length > 4}
 end
 
-def find_the_cheese
+def find_the_cheese(food)
+  # the array below is here to help	  food.find do |num|
+  cheese_types = ["cheddar", "gouda", "camembert"]	    num == "cheddar" || num == "gouda" || num == "camembert"
+end	  end
+  
 end
